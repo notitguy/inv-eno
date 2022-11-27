18a96659f139777a69f3e2e83413ee2e255bb6b3
notitguy <notitguy@email.com>
2022-11-27 22:41:17 +0100

    fix "public" path not being recognized

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -3,6 +3,11 @@ import type { AstroI18nextConfig } from "astro-i18next";
 const config: AstroI18nextConfig = {
   defaultLocale: "cs",
   locales: ["en"],
+  i18nextServer: {// because config.publicDir.pathname is not getting "public" folder in src/index.ts 77
+    backend: {
+        loadPath: "./public/locales/{{lng}}/{{ns}}.json", 
+    }
+  }
 };
 
 export default config;
