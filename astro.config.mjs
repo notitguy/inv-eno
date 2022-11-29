110488f49371f6ec1e15ba535f060b3d87fb295f
Notitguy Office <kelvin77@seznam.cz>
2022-11-29 22:40:59 +0100

    Fix build error, add snippets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 
 export default defineConfig({
+  site: 'https://enotrattoria.invitaly.cz',
   integrations: [
     astroI18next(),
   ],
