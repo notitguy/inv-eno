4b1a56ae8fcf050af798b60c5594f608aaa4c8c7
Notitguy Office <kelvin77@seznam.cz>
2022-12-01 22:13:45 +0100

    + random image hero, custom lang selector 🎉

--- src/components/islands/RandomImage.jsx
+++ src/components/islands/RandomImage.jsx
@@ -1,13 +1,13 @@
 import { h } from 'preact';
 import { useState } from 'preact/hooks';
 
-export default function RandomImage({images, alt, width, height}) {
+export default function RandomImage({images, className, alt, width, height}) {
 
   const randomImg = () => images[(Math.floor(Math.random() * images.length))];
   
   const [image] = useState(randomImg());
 
   return (
-    <img src={image} alt={alt} width={width} height={height} />
+    <img src={image} class={className} alt={alt} width={width} height={height} />
   );
 }
