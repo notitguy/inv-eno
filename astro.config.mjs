3c1d9db1a24bc6838f5ee0e8cc4fa813f9b50645
Notitguy Office <kelvin77@seznam.cz>
2022-12-01 20:03:54 +0100

    Add random image hero 🎉

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import astroI18next from "astro-i18next";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://enotrattoria.invitaly.cz',
-  integrations: [
-    astroI18next(),
-  ],
+  integrations: [astroI18next(), preact()]
 });
