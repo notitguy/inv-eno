7c85efa3b2e5b18af6f61f4979153d7086664a84
notitguy <notitguy@email.com>
2023-02-10 18:33:46 +0100

    GTM install with Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import astroI18next from "astro-i18next";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://enotrattoria.invitaly.cz',
-  integrations: [astroI18next(), preact()]
+  integrations: [astroI18next(), preact(), partytown()]
 });
