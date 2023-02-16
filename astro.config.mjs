fbdc1eed50592d2fa68092408211d42a9cbae996
notitguy <notitguy@email.com>
2023-02-16 21:00:08 +0100

    add compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,20 @@ import astroI18next from "astro-i18next";
 import preact from "@astrojs/preact";
 // import partytown from "@astrojs/partytown";
 import image from '@astrojs/image';
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://enotrattoria.invitaly.cz/',
-  integrations: [
-    astroI18next(),
-    preact(),
-    image(),
-    // partytown({
-    //   config: { 
-    //     forward: ["dataLayer.push"],
-    //   },
-    // }),
-  ],
+  integrations: [astroI18next(), preact(), image(),
+  // partytown({
+  //   config: { 
+  //     forward: ["dataLayer.push"],
+  //   },
+  // }),
+  compress({
+    css: false,
+    img: false,
+    svg: false,
+  })]
 });
