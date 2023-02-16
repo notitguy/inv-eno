d1ffa2c0e1bdf153c710092088d28acb81f15261
notitguy <notitguy@email.com>
2023-02-17 00:55:43 +0100

    font optimizer, trygin partytown again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,34 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import preact from "@astrojs/preact";
-// import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown";
 import image from '@astrojs/image';
 import compress from "astro-compress";
+// import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://enotrattoria.invitaly.cz/',
   integrations: [astroI18next(), preact(), image(),
-  // partytown({
-  //   config: { 
-  //     forward: ["dataLayer.push"],
-  //   },
-  // }),
-  compress({
-    css: false,
-    img: false,
-    svg: false,
-  })]
+    // critters(),
+    partytown({
+      config: { 
+        forward: ["dataLayer.push"],
+        resolveUrl: function (url, location, type) {
+          if (type === 'script') {
+            var proxyUrl = new URL('https://enotrattoria.invitaly.cz/');
+            proxyUrl.searchParams.append('url', url.href);
+            return proxyUrl;
+          }
+          return url;
+        },
+      },
+    }),
+    compress({
+      // html: false,
+      css: false,
+      img: false,
+      svg: false
+    })
+  ]
 });
